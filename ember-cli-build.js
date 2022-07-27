f42639b5088f4c4537a74d22030880ae6ba30b65
bureyburey <bureyburey@gmail.com>
2022-07-27 15:15:00 +0300

    replace transform-class-properties with plugin-proposal-class-properties

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,7 +10,7 @@ module.exports = function (defaults) {
     },
     babel: {
       plugins: [
-        // 'transform-class-properties',
+        '@babel/plugin-proposal-class-properties',
         'transform-react-jsx',
       ],
     },
